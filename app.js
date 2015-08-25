0f184281cdf9e1d7cdeafd510cbdd6649afb274d
Magnus Egelberg <magnus@egelberg.se>
2015-08-25 15:54:23 +0200

    -

--- app.js
+++ app.js
@@ -178,11 +178,12 @@ function runRSS(config) {
 function runFika() {
 	var rule = new schedule.RecurrenceRule();
 
-	rule.hour = 15;
-	rule.minute = 0;
+	//rule.hour = 15;
+	//rule.minute = 0;
+	rule.second = 30;
 
 	schedule.scheduleJob(rule, function() {
-		var display = new Matrix.Display();
+		var display = new matrix.Display();
 		
 		display.emoji(48, {hold:1});
 		display.emoji(48, {hold:1});
